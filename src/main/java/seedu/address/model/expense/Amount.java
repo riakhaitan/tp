9cd2e9ece243c891952d9699f35540054cbd2138
Darin Loh <43445720+talkintomato@users.noreply.github.com>
2022-03-14 20:50:22 +0800

    changed comments to be clearer

--- src/main/java/seedu/address/model/expense/Amount.java
+++ src/main/java/seedu/address/model/expense/Amount.java
@@ -33,7 +33,7 @@ public class Amount {
     }
 
     /**
-     * Returns true if a given string is a valid name.
+     * Returns true if a given string is a valid amount.
      */
     public static boolean isValidAmount(String test) {
         return test.matches(VALIDATION_REGEX) && test != "";
