a3228011ca3545be5b4a7aead4fc773ae0a1fe5b
lamwj98 <lamwj98@gmail.com>
2022-03-23 15:10:40 +0800

    Update find command message usage

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -17,7 +17,7 @@ public class FindCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all expenses whose names contain any of \n"
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie\n\n";
+            + "Example: " + COMMAND_WORD + " Cafe Grab Basketball\n\n";
 
     private final DescriptionContainsKeywordsPredicate predicate;
 
