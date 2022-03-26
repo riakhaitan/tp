44de7469266c177f95d8869da5a7186b97833a8c
Shivang211 <shivanggupta2110@gmail.com>
2022-03-26 14:43:53 +0800

    improve help command

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -1,7 +1,12 @@
 package seedu.address.logic.commands;
 
+import java.util.Objects;
+
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.ui.HelpWindow;
+
+
 
 /**
  * Format full help instructions for every command for display.
@@ -14,7 +19,15 @@ public class HelpCommand extends Command {
     public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
-            + "Example: " + COMMAND_WORD;
+            + "Example: " + COMMAND_WORD + " [COMMAND_WORD]";
+
+    private static final String INVALID_COMMAND_USAGE = "Invalid command usage";
+
+    private final String commandWordArg;
+
+    public HelpCommand(String commandWordArg) {
+        this.commandWordArg = commandWordArg;
+    }
 
     /**
      * Format help command
@@ -31,7 +44,57 @@ public class HelpCommand extends Command {
 
 
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+    public CommandResult execute(Model model) throws CommandException {
+        if (commandWordArg.trim().isEmpty()) {
+            return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        } else {
+            switch (commandWordArg.trim()) {
+            case AddCommand.COMMAND_WORD:
+                return new CommandResult(AddCommand.MESSAGE_USAGE);
+
+            case EditCommand.COMMAND_WORD:
+                return new CommandResult(EditCommand.MESSAGE_USAGE);
+
+            case DeleteCommand.COMMAND_WORD:
+                return new CommandResult(DeleteCommand.MESSAGE_USAGE);
+
+            case ClearCommand.COMMAND_WORD:
+                return new CommandResult(ClearCommand.HELP_COMMAND);
+
+            case FindCommand.COMMAND_WORD:
+                return new CommandResult(FindCommand.MESSAGE_USAGE);
+
+            case ListCommand.COMMAND_WORD:
+                return new CommandResult(ListCommand.HELP_COMMAND);
+
+            case ExitCommand.COMMAND_WORD:
+                return new CommandResult(ExitCommand.HELP_COMMAND);
+
+
+            case HelpCommand.COMMAND_WORD:
+                return new CommandResult(HelpCommand.MESSAGE_USAGE);
+
+            default:
+                throw new CommandException(INVALID_COMMAND_USAGE);
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        HelpCommand that = (HelpCommand) o;
+        return Objects.equals(commandWordArg, that.commandWordArg);
+    }
+
+    public int hashCode() {
+        return Objects.hash(commandWordArg);
     }
 }
