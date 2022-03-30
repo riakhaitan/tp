efc1c5b660803df63a2a3dcb3dce693555eacf22
Tham Jin Lin <tham.jinlin@dhs.sg>
2022-03-31 05:44:58 +0800

    no message

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -1,12 +1,10 @@
 package seedu.address.ui;
 
-import java.io.FileInputStream;
 import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.stage.Stage;
