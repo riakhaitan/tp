eb6eceea47049218eebe7e8764be8ed730273b3e
Shivang211 <shivanggupta2110@gmail.com>
2022-04-10 14:26:53 +0800

    fix error message

--- src/main/java/seedu/address/model/person/PersonName.java
+++ src/main/java/seedu/address/model/person/PersonName.java
@@ -6,9 +6,10 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class PersonName {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Person name should only consist of alphabets and digits, and it should not be blank";
+            "Person name must consist of alphabets (may contain digits as well) "
+                    + "and it should not be blank";
 
-    public static final String VALIDATION_REGEX = "^([a-zA-Z])+[a-zA-Z0-9_ ]*";
+    public static final String VALIDATION_REGEX = "^[a-zA-Z0-9_ ]*([a-zA-Z])+[a-zA-Z0-9_ ]*";
 
     public final String personName;
 
